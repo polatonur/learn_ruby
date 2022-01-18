8fed7f8e4857d9d5aa7f92a9bce949c68fdb65dd
onur <onurpolatplt@gmail.com>
2022-01-18 17:21:32 +0100

    add some string method exapmles

---
+++ string_methods.rb
@@ -0,0 +1,50 @@
+title = "onur"
+
+ 
+#👇 ==>"onur"
+title.downcase 
+
+#👇 ==>"ONUR"
+title.upcase
+
+#👇 ==>"Onur"
+title.capitalize
+
+#👇 ==>4
+title.length
+
+#👇 ==>false
+title.empty?
+
+#👇 ==>1
+title.index("n")
+
+#👇 ==>nil
+title.index("x")
+
+#👇 ==>0 first match
+"title".index(/t/)
+
+#👇 ==>name
+"title".replace("name")
+
+'bar'[2] # => "r"
+
+#👇 ==> "oindeo"
+xindex.gsub("x",o)
+
+#👇 ==>to integer
+"10".to_i
+
+#👇 ==>to floaf
+"3.14159".to_f  # => 3.14159
+
+'foo'.match?(/o/) # => true
+
+'stressed'.reverse # => "desserts"
+
+
+
+
+
+
