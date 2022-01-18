ca4e42a62f0c6c6912728d8ad24a3d12b483a8c9
onur <onurpolatplt@gmail.com>
2022-01-18 17:21:45 +0100

    add new file

---
+++ variables.rb
@@ -0,0 +1,33 @@
+# variable always snake case foo_bar
+my_variable = "Hello world"
+
+# Uppercase characters for constants
+API_KEY = "qwerty"
+
+# types in ruby 👇 
+# 1=>string
+first_name = "onur"  # first_name.class =>string
+ 
+# 2=>number
+year = 2022 # year.class =>number
+
+# 3=>float
+pi = 3.14 # pi.class =>number
+
+# 4=>boolean
+is_done = true #is_done.class = Trueclass
+
+# hash {}
+user = {}
+
+# symbols
+
+# {:name=>"onur", :age=>30 :city=>"paris"}
+user = {
+    name:"onur",
+    age:30,
+    city:"Paris"
+}
+
+# arrays
+list [1,2,3,4,5,6,7]
