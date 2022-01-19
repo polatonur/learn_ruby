f536cf8f4671d9763155f96820cecce0f0a8f159
onur <onurpolatplt@gmail.com>
2022-01-19 16:15:03 +0100

    add some integer methods

---
+++ numbers.rb
@@ -0,0 +1,8 @@
+number = 4
+puts 4 / 3 # return 0 1.33 rounded ==> 1 no float
+
+number = 10.to_f # fload one decimal
+puts number
+
+# "%f.2" % number float number with 2 decimals
+puts '%.2f' % number
